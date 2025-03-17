eb004ec04345a938fe06e2ff09638a680cc0bd22
evidhya60@gmail.com <evidhya60@gmail.com>
2025-03-17 16:15:59 +0530

    log4j implementation

--- src/main/java/com/careeropenings/CareerOpenings/Controller/RetrieveAllCandidateController.java
+++ src/main/java/com/careeropenings/CareerOpenings/Controller/RetrieveAllCandidateController.java
@@ -2,6 +2,8 @@ package com.careeropenings.CareerOpenings.Controller;
 
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +13,8 @@ import com.careeropenings.CareerOpenings.Service.RetrieveAllCandidatesService;
 
 @RestController
 public class RetrieveAllCandidateController {
-
+	private static final Logger logger = LoggerFactory.getLogger(RetrieveAllCandidateController.class);
+	
 	private final RetrieveAllCandidatesService retrieveService;
 
 	public RetrieveAllCandidateController(RetrieveAllCandidatesService retrieveService) {
@@ -20,7 +23,9 @@ public class RetrieveAllCandidateController {
 
 	@PostMapping("/allCandidate")
 	public Map<String, Object> retrieveAllCandidates() {
+		logger.info("RetrieveAllCandidateController starts the retrieveAllCandidates service");
 		Map<String, Object> allCandidate = retrieveService.retrieveAllCandidate();
+		logger.info("RetrieveAllCandidateController ends the retrieveAllCandidates service" + allCandidate);
 		return allCandidate;
 	}
 
