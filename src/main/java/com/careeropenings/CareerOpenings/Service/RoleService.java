29a18d9e971357c4299cd1052010a1d4d1b9cb3c
evidhya60@gmail.com <evidhya60@gmail.com>
2025-03-19 16:43:19 +0530

    sonar changes

--- src/main/java/com/careeropenings/CareerOpenings/Service/RoleService.java
+++ src/main/java/com/careeropenings/CareerOpenings/Service/RoleService.java
@@ -4,8 +4,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import com.careeropenings.CareerOpenings.Entity.RoleDetails;
@@ -13,8 +11,6 @@ import com.careeropenings.CareerOpenings.Repo.RoleRepo;
 
 @Service
 public class RoleService {
-	
-	private static final Logger logger = LoggerFactory.getLogger(RoleService.class);
 	
 	private final RoleRepo roleRepo;
 
